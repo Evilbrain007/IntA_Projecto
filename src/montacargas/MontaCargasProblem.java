1978129f14b8597e0b8b1d046a89f296f5c6f80c
soraia <soraiapratesbasso@gmail.com>
2017-04-23 18:58:09 +0000

    Adicionei classe MontaCargasProblem

---
+++ src/montacargas/MontaCargasProblem.java
@@ -0,0 +1,20 @@
+package montacargas;
+
+import agent.Problem;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sorai on 23-Apr-17.
+ */
+public class MontaCargasProblem extends Problem<MontaCargasState> {
+
+    //o estado final apenas interessa quando a coluna do forklift é a última
+    //private MontaCargasState goalState;
+
+    public MontaCargasProblem(MontaCargasState initialState) {
+        //TODO
+      // super(initialState, new ArrayList<>(4));
+
+    }
+}
